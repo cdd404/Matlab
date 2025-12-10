a3c14b7b19ddaeaeae97e985627a228725f922e3
cdd404 <davidovwilliam@gmail.com>
2025-12-10 09:57:27 +0530

    Update Os6a.c

--- Os6a.c
+++ Os6a.c
@@ -1,40 +1,55 @@
-#include<stdio.h>
-int main()
-{
-int fragments[10], blocks[10], files[10];
-int m, n, number_of_blocks, number_of_files, temp, top=0;
-static int block_arr[10], file_arr[10];
-printf("\nEnter the Total Number of Blocks:");
-scanf("%d", &number_of_blocks);
-printf("Enter the Total Number of Files:");
-scanf("%d", &number_of_files);
-printf("\nEnter the Size of the Blocks\n");
-for(m=0; m < number_of_blocks; m++)
-{
-printf("Block No.[%d]:", m+1);
-scanf("%d", &blocks[m]);
-}
-printf("Enter the Size of the Files:\n");
-for(m=0; m < number_of_files; m++)
-{
-printf("File No.[%d]:", m+1);
-scanf("%d", &files[m]);
-}
-for(m=0; m < number_of_files; m++)
-{
-for(n=0; n < number_of_blocks; n++)
-{
-if(block_arr == 1)
-continue;
-temp = blocks - files[m];
-if(temp >= 0)
-{
-if(top < temp)
-{
-file_arr[m] = n;
-top = temp;
-}
-}
+#include <stdio.h>
+
+int main() {
+    int fragments[10], blocks[10], files[10];
+    int m, n, number_of_blocks, number_of_files, temp, top = 0;
+    static int block_arr[10], file_arr[10];
+
+    printf("\nEnter the Total Number of Blocks: ");
+    scanf("%d", &number_of_blocks);
+
+    printf("Enter the Total Number of Files: ");
+    scanf("%d", &number_of_files);
+
+    printf("\nEnter the Size of the Blocks\n");
+    for (m = 0; m < number_of_blocks; m++) {
+        printf("Block No.[%d]: ", m + 1);
+        scanf("%d", &blocks[m]);
+    }
+
+    printf("\nEnter the Size of the Files:\n");
+    for (m = 0; m < number_of_files; m++) {
+        printf("File No.[%d]: ", m + 1);
+        scanf("%d", &files[m]);
+    }
+
+    for (m = 0; m < number_of_files; m++) {
+        for (n = 0; n < number_of_blocks; n++) {
+            if (block_arr[n] == 1)   // check if block already allocated
+                continue;
+
+            temp = blocks[n] - files[m];
+            if (temp >= 0) {
+                if (top < temp) {
+                    file_arr[m] = n;
+                    top = temp;
+                }
+            }
+        }
+        fragments[m] = top;
+        block_arr[file_arr[m]] = 1;  // mark block as allocated
+        top = 0;
+    }
+
+    printf("\nFile Number\tFile Size\tBlock Number\tBlock Size\tFragment");
+    for (m = 0; m < number_of_files; m++) {
+        printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",
+               m + 1, files[m], file_arr[m] + 1, blocks[file_arr[m]], fragments[m]);
+    }
+    printf("\n");
+
+    return 0;
+}}
 }
 fragments[m] = top;
 block_arr[file_arr[m]] = 1;
